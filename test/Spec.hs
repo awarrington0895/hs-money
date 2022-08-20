49dd9c8f37dceb8ec56f85c61dba73da113f7afa
Alex <awarrington0895@gmail.com>
2022-08-20 02:32:09 +0000

    Reduce tests needed

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,14 @@
 import Test.HUnit
 import Money
 
-
-fiveDollars :: Dollar 
-fiveDollars = Dollar 5
-
 testMultiplication :: Test
-testMultiplication = TestCase $ (assertEqual "$5 * 2 = $10" (times fiveDollars 2) (Dollar 10))
+testMultiplication = "testMultiplication" ~: do 
+                     five <- getDollar 5
+                     assertEqual "$5 * 2 = $10" (times five 2) (Dollar 10)
+                     assertEqual "$5 * 3 = $15" (times five 3) (Dollar 15)
+
+getDollar :: Int -> IO Dollar
+getDollar x = return (Dollar x)
 
 tests :: Test
 tests = TestList 
