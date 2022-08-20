21ed8441196ea0f8bed797979b42f7dc74012e82
Alex <awarrington0895@gmail.com>
2022-08-20 01:49:08 +0000

    Set up Test Cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+
+foo :: Int -> (Int, Int)
+foo x = (1, 2)
+
+test1 :: Test
+test1 = TestCase $ (assertEqual "for (foo 3)," (1,2) (foo 3))
+
+tests :: Test
+tests = TestList 
+    [ test1
+    ]
+
+main :: IO Counts
+main = runTestTT tests
